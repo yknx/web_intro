e0e41726dee7110f0d6cca5c2deb908e160f4fa2
Yekyung Nah <nyk7535@snu.ac.kr>
2023-04-04 11:54:40 +0900

    0404
    23/04/04

---
+++ javascript/module/app.mjs
@@ -0,0 +1,7 @@
+import * as math from "./math.mjs";
+
+console.log(math.add(1,2));
+console.log(math.subtract(5,3));
+console.log(math.divide(1,2));
+console.log(math.multiply(4,2));
+console.log(math.pow(2,6));
---
+++ javascript/module/math.mjs
@@ -0,0 +1,20 @@
+export function add(x,y) {
+    return x+y;
+}
+
+export function subtract(x,y) {
+    return x-y;
+}
+
+export function multiply(x,y) {
+    return x*y;
+}
+
+export function divide(x,y) {
+    return x/y;
+}
+
+export function pow(x,y) {
+    return x**y;
+}
+
